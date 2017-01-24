6a602a843013bda8aee993d26d572e3ce077104c
Andrew Kubera <andrewkubera@gmail.com>
2017-01-24 00:54:53 -0500

    setup.py: Setup loads __meta__.py to get version info & other metadata

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 #
 # setup.py
 #
@@ -6,14 +7,12 @@
 from setuptools import setup, find_packages
 from importlib.machinery import SourceFileLoader
 
-meta = SourceFileLoader("meta", "aberdeen/__meta__.py").load_module()
-
 desc = "Conversion from markdown files to database entries to use as the backend of a blog"
 
 REQUIRES = [
     'emoji',
     'termcolor2',
-    'python-dateutil'
+    'python-dateutil',
 ]
 
 OPTIONAL_REQUIRES = {
@@ -26,7 +25,7 @@ KEYWORDS = [
     'blog',
     'publishing',
     'nosql',
-    'mongodb'
+    'mongodb',
 ]
 
 CLASSIFIERS = [
@@ -40,17 +39,20 @@ CLASSIFIERS = [
 
 LICENSE = 'Apache 2.0'
 
-tar_url = 'https://github.com/akubera/aberdeen/archive/v%s.tar.gz' % (aberdeen.__version__)
+meta = SourceFileLoader("meta", "aberdeen/__meta__.py").load_module()
+
+tar_url = 'https://github.com/akubera/aberdeen/archive/v%s.tar.gz' % (meta.version)
+
 
 setup(
     name="aberdeen",
     packages=find_packages(exclude=['test']),
-    version=aberdeen.__version__,
+    version=meta.version,
     description=desc,
-    url=aberdeen.__homepage__,
+    url=meta.url,
     download_url=tar_url,
-    author=aberdeen.__author__,
-    author_email=aberdeen.__contact__,
+    author=meta.author,
+    author_email=meta.author_email,
     keywords=KEYWORDS,
     license=LICENSE,
     classifiers=CLASSIFIERS,
